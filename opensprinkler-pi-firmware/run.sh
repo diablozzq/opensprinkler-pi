3d9a4be1e4afac2b48962cadbdfbe6d2ac36bb91
Kasey Morland <morlandkc@gmail.com>
2026-04-19 19:08:50 -0500

    Fixes from gemini

--- opensprinkler-pi-firmware/run.sh
+++ opensprinkler-pi-firmware/run.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::log.info "Starting OpenSprinkler Pi firmware..."
+bashio::log.info "Configuring data persistence..."
 
 cd /opensprinkler
+
+# Persist config and log files
+for file in nvm.dat progs.dat stns.dat log.json logs.sqlite; do
+    if [ ! -f "/data/$file" ]; then
+        # Initialize if it doesn't exist in /data
+        touch "/data/$file"
+    fi
+    # Link it back to the OpenSprinkler directory
+    ln -sf "/data/$file" "/opensprinkler/$file"
+done
+
+bashio::log.info "Starting OpenSprinkler Pi firmware..."
 exec ./OpenSprinkler
